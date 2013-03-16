5a41408caca95f7ddca39453d09d8118b165ebed
panchicore <thepanchi@gmail.com>
2013-03-16 12:20:13 -0500

    test

--- elbtest/views.py
+++ elbtest/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from random import randint
 
 def home(request):
     return render(request, 'home/index.html', {'r': str(request)})
@@ -11,5 +11,28 @@ def ping(request):
 
 
 def pong(request):
-    return HttpResponse('pong')
+    for a in range(1, 1000):
+        primes(randint(a, 9999))
+    return HttpResponse(primes(randint(1, 10)))
 
+
+def primes(n):
+    if n==2:
+        return [2]
+    elif n<2:
+        return []
+    s=range(3,n+1,2)
+    mroot = n ** 0.5
+    half=(n+1)/2-1
+    i=0
+    m=3
+    while m <= mroot:
+        if s[i]:
+            j=(m*m-3)/2
+            s[j]=0
+            while j<half:
+                s[j]=0
+                j+=m
+        i=i+1
+        m=2*i+3
+    return [2]+[x for x in s if x]
